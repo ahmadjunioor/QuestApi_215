05932c99e17f8c8c9d0d1f5583296e89295b77da
ahmadjunioor <161430813+ahmadjunioor@users.noreply.github.com>
2025-12-18 20:07:21 +0700

    feat: menambahkan definisi rute dan resource title untuk destinasi utama

---
+++ app/src/main/java/com/example/questapi_215/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.questapi_215.uicontroller.route
+
+import com.example.questapi_215.R
+import com.example.questapi_215.uicontroller.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
