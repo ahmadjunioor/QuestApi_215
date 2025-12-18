71258889d00936cd6b58809bc6bbda3e63ac1239
ahmadjunioor <161430813+ahmadjunioor@users.noreply.github.com>
2025-12-18 19:55:14 +0700

    membuat file DestinasiEntry.kt dan isinya

---
+++ app/src/main/java/com/example/questapi_215/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.questapi_215.uicontroller.route
+
+import com.example.questapi_215.R
+import com.example.questapi_215.uicontroller.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
