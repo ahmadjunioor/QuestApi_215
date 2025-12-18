4f7c2f39ced5b5b9ada45bc9cb137064250e800d
ahmadjunioor <161430813+ahmadjunioor@users.noreply.github.com>
2025-12-18 15:21:55 +0700

    membuat dan mengisi file RepositoryDataSiswa.kt

--- app/src/main/java/com/example/questapi_215/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_215/repositori/RepositoryDataSiswa.kt
@@ -1,2 +1,15 @@
 package com.example.questapi_215.repositori
 
+import com.example.questapi_215.apiservice.ServiceApiSiswa
+import com.example.questapi_215.modeldata.DataSiswa
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa() : List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+) : RepositoryDataSiswa {
+    override suspend fun getDataSiswa() : List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
