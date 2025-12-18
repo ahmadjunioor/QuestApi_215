9e365d06b2f88b13dc8788392011ed041f30679b
ahmadjunioor <161430813+ahmadjunioor@users.noreply.github.com>
2025-12-18 20:12:00 +0700

    memperbaiki isi MainActivity.kt

--- app/src/main/java/com/example/questapi_215/MainActivity.kt
+++ app/src/main/java/com/example/questapi_215/MainActivity.kt
@@ -7,11 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.questapi_215.ui.theme.QuestApi_215Theme
+import com.example.questapi_215.uicontroller.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +19,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestApi_215Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestApi_215Theme {
-        Greeting("Android")
     }
 }
